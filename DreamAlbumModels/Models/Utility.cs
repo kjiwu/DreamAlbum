dcb2d77c70f4a8abb9e85f19b921485e47c6c80b
kjiwu <kjiwu@hotmail.com>
2013-12-24 22:28:36 +0800

    add slide show page

---
+++ DreamAlbumModels/Models/Utility.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamAlbumModels.Models
+{
+    public class Utility
+    {
+        public static Stream GetMediaLibraryImageStream(string name)
+        {
+            MediaLibrary library = new MediaLibrary();
+            var picture = (from pic in library.Pictures
+                          where pic.Name.Equals(name)
+                          select pic).First();
+            if (null != picture)
+            {
+                return picture.GetImage();
+            }
+
+            return null;
+        }
+    }
+}
